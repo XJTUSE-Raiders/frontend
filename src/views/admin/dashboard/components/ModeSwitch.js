afca8a09651e9b89e5d99f5e386f304c2c66603e
Zhiyuan Xiang <zy.xiang.aca@outlook.com>
2024-01-09 11:29:16 +0800

    fixed traffic mode button

--- src/views/admin/dashboard/components/ModeSwitch.js
+++ src/views/admin/dashboard/components/ModeSwitch.js
@@ -6,25 +6,35 @@ import {
   useColorModeValue,
   Text
 } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function ModeSwitch (props) {
   const textColor = useColorModeValue('navy.700', 'white')
-  const { isTraffic } = props
+  const [trafficMode, setTrafficMode] = useState(false)
+  function checkStatus (mode) {
+    setTrafficMode(!mode)
+    console.log(trafficMode)
+  }
   return (
     <Flex align='center' mb={2}>
-      <FormControl
-        display='flex'
-        alignItems='center'
-        onClick={e => {
-          e.stopPropagation()
-        }}
-      >
+      <FormControl display='flex' alignItems='center' onClick={e => {
+            e.stopPropagation();
+            checkStatus(trafficMode)
+          }}>
         <FormLabel htmlFor='traffic' mb='0' color>
-          <Text color={textColor} size='sm' fontWeight='bold'>Visits</Text>
+          <Text color={textColor} size='sm' fontWeight='bold'>
+            Visits
+          </Text>
         </FormLabel>
-        <Switch isChecked={isTraffic} id='traffic' />
+        <Switch
+          isChecked={trafficMode}
+          id='traffic'
+
+        />
         <FormLabel htmlFor='traffic' mb='0' ml={1}>
-        <Text color={textColor} size='sm' fontWeight='bold'>Traffic</Text>
+          <Text color={textColor} size='sm' fontWeight='bold'>
+            Traffic
+          </Text>
         </FormLabel>
       </FormControl>
     </Flex>
